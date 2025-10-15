a7c1f2989512b101ff4db01720fff40d0b509b15
pleaseoffme404 <sandovalcarmonaa.fernando@gmail.com>
2025-10-14 20:47:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const session = require('express-session')
 require('dotenv').config();
 
-const app=express()
-const PORT = process.env.PORT
 
+const app=express()
+const PORT = process.env.PORT|| 3000;
+app.use(express.json());
+  
 app.use(
     session({
       secret: process.env.SESSION_SECRET, 
@@ -40,7 +42,21 @@ app.use(
       }
     });
   });
+app.post('/login',(req,res)=>{
+    const {usuario, password}=req.body
+    if(usuario=='admin'&& password ==='1234'){
+        res.send('Inicio de sesion correcto')
+    }else{
+        res.status(401).send('Credenciales incorrectas')
+    }
+})
 
+app.get('/cerrar',(req, res)=>{
+    req.session.destroy(err=>{
+        if(err)return res.status(500).send('Error al cerrar sesion')
+            res.send('sesion cerrada correctamente')
+    })
+})
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor funcionando en http://localhost:${PORT}/main`);
+    console.log(` Servidor funcionando en http://localhost:${PORT}/main`);
   });
